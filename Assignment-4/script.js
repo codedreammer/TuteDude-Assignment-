577232365b8b1474028f23ced0f69b295be6151e
Akshay Mehta <nkanal38@gmail.com>
2025-11-12 09:07:43 +0530

    add the js

---
+++ Assignment-4/script.js
@@ -0,0 +1,33 @@
+const greeting = document.getElementById("greeting");
+const nameInput = document.getElementById("nameInput");
+const greetBtn = document.getElementById("greetBtn");
+
+    greetBtn.addEventListener("click", () => {
+    const name = nameInput.value.trim();
+    if (name === "") {
+        greeting.textContent = "Hello";
+    } else {
+        greeting.textContent = `Hello, ${name}!`;
+    }
+    });
+
+    const boxes = document.querySelectorAll(".box");
+
+    boxes.forEach(box => {
+        box.addEventListener('mouseover', () => {
+            if (box.textContent === 'Red') {
+                box.style.backgroundColor = 'red';
+            } else if (box.textContent === 'Green') {
+                box.style.backgroundColor = 'green';
+            } else if (box.textContent === 'Blue') {
+                box.style.backgroundColor = 'blue';
+            } else if (box.textContent === 'Yellow') {
+                box.style.backgroundColor = 'yellow';
+            }
+        });
+        
+        box.addEventListener('mouseout', () => {
+            box.style.backgroundColor = 'white';
+            box.style.color = 'black';
+        });
+    });
